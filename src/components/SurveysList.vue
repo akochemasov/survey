2c5c1f3a76370c1bfb1e9f57bd14897e153238f5
Alexander Kochemasov <akochemasov@gmail.com>
2023-01-27 21:58:23 +0100

    Переверстал карточки опросов

--- src/components/SurveysList.vue
+++ src/components/SurveysList.vue
@@ -1,20 +1,35 @@
 <template>
   <v-container>
-    <v-row v-for="(item, index) in surveys" :key="index" align="center">
-      <v-col class="mx-auto" width="100%">
-        <v-card outlined shaped elevation="4">
-          <v-card-title>{{ item.title }}</v-card-title>
-          <v-card-text>От {{ item.author }}</v-card-text>
-          <v-card-actions>
-            <v-btn color="primary" @click="() => {}">Подробнее</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+    <template :key="index" v-for="(item, index) in surveys">
+      <v-row align="center">
+        <v-col class="mx-auto" width="100%">
+          <v-hover v-slot="{ isHovering, props }">
+            <v-card
+              outlined
+              :elevation="isHovering ? 6 : 2"
+              v-bind="props"
+              @click="onClick(item)"
+            >
+              <div class="d-flex justify-space-between align-center">
+                <div>
+                  <v-card-title>{{ item.title }}</v-card-title>
+                  <v-card-text>От {{ item.author }}</v-card-text>
+                </div>
+                <div class="d-flex align-center mr-5">
+                  <v-avatar :icon="mdiThumbUpOutline" />
+                  <div class="text-justify">{{ item.totalVoices }}</div>
+                </div>
+              </div>
+            </v-card>
+          </v-hover>
+        </v-col>
+      </v-row>
+    </template>
   </v-container>
 </template>
 
 <script setup lang="ts">
+import { mdiThumbUpOutline } from "@mdi/js";
 import { ref, defineProps, watchEffect } from "vue";
 import { getSurveys } from "@/services/api";
 import type { ISurvey } from "@/types/survey";
@@ -27,6 +42,10 @@ const surveys = ref<ISurvey[]>([]);
 watchEffect(async () => {
   surveys.value = await getSurveys({ type: props.type });
 });
+
+const onClick = (item: ISurvey) => {
+  console.log(item);
+};
 </script>
 
 <style scoped module></style>
